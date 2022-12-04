4c233234953f91282454cb4e5674a2db5c93b9cd
universallyleo <omgleo@protonmail.com>
2022-12-04 23:25:55 +0900

    github action test

---
+++ gha_test.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+from io import open as iopen  # to make sure we are not using File.open
+import json
+
+src = "https://raw.githubusercontent.com/universallyleo/ngzkHandshakeTable/main/src/lib/data/data.json"
+
+scrappage = requests.get(src)
+if scrappage.status_code != 200:
+    print(f"ERROR {scrappage.status_code}: Link ({src}) requset fail")
+    sys.exit()
+
+fromfile = str("./31st.json")
+fromdata = json.load(iopen(fromfile, encoding="utf-8"))
+
+tofile = str("./src/data.json")
+todata = json.load(iopen(tofile, encoding="utf-8"))
+todata.append(fromdata)
+
+print("OK")
