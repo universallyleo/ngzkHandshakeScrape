ae150af697b27d92496980a12c3527c5645a1dd1
universallyleo <omgleo@protonmail.com>
2022-12-04 23:43:59 +0900

    update action update pyscript

--- gha_test.py
+++ gha_test.py
@@ -13,8 +13,11 @@ if scrappage.status_code != 200:
 fromfile = str("./31st.json")
 fromdata = json.load(iopen(fromfile, encoding="utf-8"))
 
-tofile = str("./src/data.json")
+tofile = f".src/data_{sys.argv[1]}.json"
 todata = json.load(iopen(tofile, encoding="utf-8"))
 todata.append(fromdata)
+
+with iopen(str(f".src/data.json"), "w") as outfile:
+    json.dump(todata, outfile, indent=3)
 
 print("OK")
